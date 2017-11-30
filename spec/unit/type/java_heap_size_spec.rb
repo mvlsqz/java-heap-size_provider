29cf516312582a5f07dd8be4b6168d523c46ecbb
Marvin Velásques <immavlgt@gmail.com>
2017-11-29 23:22:05 -0600

    validating support to ensure property

--- spec/unit/type/java_heap_size_spec.rb
+++ spec/unit/type/java_heap_size_spec.rb
@@ -1,3 +1,4 @@
+# unit test for java_heap_size
 require 'spec_helper'
 require 'pp'
 java_heap_size = Puppet::Type.type(:java_heap_size)
@@ -12,6 +13,18 @@ describe java_heap_size do
     %i[ensure argument value].each do |property|
       it "expect to contain #{property} property" do
         expect(java_heap_size.attrtype(property)).to eq(:property)
+      end
+    end
+  end
+  describe 'validating resource attributes' do
+    %i[present absent].each do |attribute|
+      it "should support #{attribute}" do
+        expect {
+          java_heap_size.new({
+            :name => '/opt/text.txt',
+            :ensure => attribute
+          })
+        }.to_not raise_error
       end
     end
   end
